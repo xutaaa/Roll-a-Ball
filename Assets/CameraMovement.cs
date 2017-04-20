f426d6dee674b5c3540744cb73645aad67eb4e94
admin\user <xuta.liparteliani@gmail.com>
2017-04-20 20:58:06 +0400

    Creating a New game (Roll a ball) adjusting player movment and camera

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+    Vector3 distanceFromTarget;
+
+
+	void Start ()
+    {
+
+        distanceFromTarget = transform.position - target.position;
+
+    }
+	
+	
+	void Update ()
+    {
+        transform.position = target.position + distanceFromTarget;
+	}
+}
---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+    Vector3 deltaPos;
+    //public Transform player;
+    public Rigidbody playerBody;
+    public float speedMultiplier;
+
+
+
+	void Start ()
+    {
+        //deltaPos = Vector3.zero;
+
+	}
+	
+	
+	void Update ()
+    {
+        //Debug.Log( Input.GetKey(KeyCode.DownArrow) );
+        //Debug.Log(Input.GetAxis("Vertical") );
+
+        //transform.position += deltaPos;
+        // igivea rac transform.position = transform.position + deltaPos;
+
+        deltaPos.x = Input.GetAxis("Horizontal");
+        deltaPos.z = Input.GetAxis("Vertical");
+
+        
+
+
+
+
+    }
+    void FixedUpdate ()
+    {
+        //playerBody.position = playerBody.position + deltaPos;
+        playerBody.AddForce(deltaPos * speedMultiplier);
+    }
+
+}
